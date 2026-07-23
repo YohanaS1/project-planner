4f08f7ea9755ff1e4046725d9ebf489e5c959dbb
Yohana Sibhatu <sibhatu.yohana1@gmail.com>
2026-07-23 15:57:31 -0500

    total projects metric and project name display fixed

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 
 import streamlit as st
-
+# Header
 st.header("Project Planner")
 st.write("to get started, add a project to the dashboard")
+
+#storing projects in session state
 if 'projects' not in st.session_state:
     st.session_state['projects']=[]
+#creating colums for metrics
 col1, col2, col3, col4 = st.columns(4)
-total_tasks = 0
-in_progress = 0
-overdue = 0
-completed = 0
-col1.metric("Total Tasks", f"{total_tasks}")
-col2.metric("In Progress", f"{in_progress}")
-col3.metric("Overdue", f"{overdue}")
-col4.metric("Completed", f"{completed}")
+#project submit form
 with st.form(key = "Add Project", clear_on_submit=True):
     project_name = st.text_input("Project Name")
     submitted = st.form_submit_button("Submit")
@@ -24,9 +20,36 @@ with st.form(key = "Add Project", clear_on_submit=True):
         else:
             st.warning('Project name cannot be empty!', icon="⚠️")
 
+#progress metrics
+total_projects = len(st.session_state.projects)
+in_progress = 0
+overdue = 0
+completed = 0
+col1.metric("Total Projects", f"{total_projects}")
+col2.metric("In Progress", f"{in_progress}")
+col3.metric("Overdue", f"{overdue}")
+col4.metric("Completed", f"{completed}")
+
+#displaying projects
+left, middle, right = st.columns(3)
+counter = 1
 for project in st.session_state.projects:
-    with st.container():
-        st.write(f"Project: {project}")
+    if counter ==1:
+        with left:
+            container = st.container(border = True)
+            container.write(project)
+    elif counter ==2:
+        with middle:
+            container = st.container(border = True)
+            container.write(project)
+    else:
+        with right:
+            container = st.container(border = True)
+            container.write(project)
+    counter +=1
+    if counter >3:
+        counter = 1
+        
 
 
 
